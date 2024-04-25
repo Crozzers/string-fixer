fcb41c3fa49810ebd8fe348fff8758a42c0eb288
Crozzers <captaincrozzers@gmail.com>
2024-04-25 22:36:33 +0100

    Automate more of the release pipeline
    Edit changelog headings and links, bump version in package.json

---
+++ .github/scripts/edit-changelog.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+version="$1"
+TAG_PREFIX="$2"
+
+prevVersion=$(
+    cat CHANGELOG.md | \
+    # get lines with version nums on them
+    grep '## \[[0-9].*\] - [0-9]\{4\}\(-[0-9]\{2\}\)\{2\}' | \
+    # extract version number
+    grep -o '[0-9]\+\(\.[0-9]\+\)\{2\}' | \
+    # get latest one
+    head -n 1
+)
+
+# add new header for new version
+awk -v version="$version" -v date="$(date +'%Y-%m-%d')" '
+    1;/## \[Unreleased\]/{ print "\n\n## [" version "] - " date }
+' CHANGELOG.md > CHANGELOG.md.temp
+
+mv CHANGELOG.md.temp CHANGELOG.md
+
+# update links
+sed -ri 's|(\[unreleased\]: .*compare\/'$TAG_PREFIX')(.*)(\.\.\.HEAD)|\1'$version'\3|g' CHANGELOG.md
+
+awk -v prev="$TAG_PREFIX$prevVersion" -v version="$TAG_PREFIX$version" \
+    -v repo="$GITHUB_REPOSITORY" '
+    1;/\[unreleased\]: /{
+        print "[" version "]: https://github.com/Crozzers/string-fixer/compare/" prev "..." version
+    }
+    ' CHANGELOG.md > CHANGELOG.md.temp
+
+mv CHANGELOG.md.temp CHANGELOG.md
--- extensions/vscode/CHANGELOG.md
+++ extensions/vscode/CHANGELOG.md
@@ -1,11 +1,19 @@
 # Change Log
 
-## [0.0.2] - 24.04.2024
+## [Unreleased]
+
+
+## [0.0.2] - 2024-04-24
 
 ### Fixed
 
 - Fixed `string-fixer` command invocation failing due to invalid cwd being set
 
-## [0.0.1] - 23.04.2024
+## [0.0.1] - 2024-04-23
 
 - Initial release
+
+
+[unreleased]: https://github.com/Crozzers/string-fixer/compare/vscode/0.0.2...HEAD
+[0.0.2]: https://github.com/Crozzers/string-fixer/compare/vscode/0.0.1...vscode/0.0.2
+[0.0.1]: https://github.com/Crozzers/string-fixer/releases/tag/vscode/0.0.1
